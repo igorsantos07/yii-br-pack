12b59a98f91b0e53c7839eb1a79bfb7a3d6e7659
Igor Santos <igorsantos07@gmail.com>
2014-05-17 00:26:14 -0300

    Adding phone validator

---
+++ PhoneValidator.php
@@ -0,0 +1,188 @@
+<?php
+/**
+ * @link    https://github.com/igorsantos07/yii-br-validator
+ * @license https://github.com/igorsantos07/yii-br-validator/blob/master/LICENSE
+ */
+
+/**
+ * PhoneValidator checks if the attribute value is a valid brazilian phone number.
+ *
+ * @author Igor Santos <igorsantos07@gmail.com>
+ */
+class PhoneValidator extends CValidator {
+
+	const TYPE_LANDLINE = 'landline';
+
+	const TYPE_CELLPHONE = 'cellphone';
+
+	const TYPE_BOTH = 'both';
+
+	public $allowEmpty;
+
+	public $ninthDigitMessage = 'Falta o nono dígito no {attribute}.';
+
+	public $lengthMessage = '{attribute} de tamanho inválido.';
+
+	public $areaMessage = 'Código de área inválido no {attribute}.';
+
+	/**
+	 * If it should validate only cellphones, landlines, or both are okay.
+	 * Should use one of PhoneValidator::TYPE_* constants.
+	 * @var string
+	 */
+	public $type = self::TYPE_BOTH;
+
+	/**
+	 * If true will also validate with the area code in the attribute. Defaults to false.
+	 * @see $areaCodeField
+	 * @var bool
+	 */
+	public $areaCode = false;
+
+	/**
+	 * If set, will use the given attribute name as area code to validate cellphones.
+	 * @var string
+	 */
+	public $areaCodeAttribute;
+
+	/**
+	 * Landline numbers begin with this list of digits.
+	 * @var array
+	 */
+	public $landlineBegin = array(2, 3);
+
+	/**
+	 * Cellphone numbers begin with this list of digits.
+	 * @var array
+	 */
+	public $cellphoneBegin = array(7, 8, 9);
+
+	/**
+	 * Cellphone numbers that require 9 digits begin with this list of digits.
+	 * @see $ninthDigitAreaCodes
+	 * @var array
+	 */
+	public $cellphoneNinthDigitBegin = array(8, 9);
+
+	/**
+	 * List of area codes that require 9 digits on the cellphone. Should be updated from time to time.
+	 * @link http://portal.embratel.com.br/embratel/9-digito/
+	 * @see  $cellphoneNinthDigitBegin
+	 */
+	public $ninthDigitAreaCodes = array(
+		11, 12, 13, 14, 15, 16, 17, 18, 19,
+		21, 22, 24, 27, 28,
+	);
+
+	/**
+	 * Temp area for found errors in sub-validation methods.
+	 * @var array
+	 */
+	protected $errors = array();
+
+	public function __construct() {
+		if ($this->message === null) {
+			$this->message = Yii::t('yii', '{attribute} is invalid.');
+		}
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function validateAttribute($object, $attribute) {
+		if (!$object->$attribute && $this->allowEmpty)
+			return;
+
+		if (!in_array($this->type, array(self::TYPE_BOTH, self::TYPE_CELLPHONE, self::TYPE_LANDLINE)))
+			throw new CException('Validator expects "type" property to be one of PhoneValidator::TYPE_* constants');
+
+		$number = preg_replace('/[^0-9_]/', '', $object->$attribute);
+
+		if ($this->areaCode) {
+			$area   = substr($number, 0, 2);
+			$number = substr($number, 2);
+		}
+		elseif ($this->areaCodeAttribute) {
+			$area = $object->{$this->areaCodeAttribute};
+		}
+		else {
+			$area = null;
+		}
+
+		if ($area && (strlen($area) != 2 || $area < 11))
+			$this->addError($object, $attribute, $this->areaMessage);
+
+		if (!$this->{'validate'.ucfirst($this->type)}($area, $number)) {
+			$total = sizeof($this->errors);
+			for ($i = 0; $i < $total; $i++)
+				$this->addError($object, $attribute, Yii::t('yii', array_shift($this->errors)));
+		}
+	}
+
+	protected function validateLandline($area, $number) {
+		if (strlen($number) != 8) {
+			$this->errors[] = $this->lengthMessage;
+			return false;
+		}
+
+		if (!in_array($number[0], $this->landlineBegin)) {
+			$this->errors[] = $this->message;
+			return false;
+		}
+
+		return true;
+	}
+
+	protected function validateCellphone($area, $number) {
+		$length     = strlen($number);
+		$ninthDigit = $area && in_array($area, $this->ninthDigitAreaCodes);
+		$dontHaveNinthDigit = array_diff($this->cellphoneBegin, $this->cellphoneNinthDigitBegin);
+
+		if ($ninthDigit && $length == 8 && in_array($number[0], $this->cellphoneNinthDigitBegin)) {
+			$this->errors[] = $this->ninthDigitMessage;
+			return false;
+		}
+		elseif (($ninthDigit && $length != 9 && in_array($number[0], $this->cellphoneNinthDigitBegin)) ||
+			   (!$ninthDigit && (
+			       ($area && $length != 8 && in_array($number[0], $this->cellphoneBegin)) ||
+			       (!$area && ($length != 8 && $length != 9))
+			   ))
+		) {
+			$this->errors[] = $this->lengthMessage;
+			return false;
+		}
+		elseif ($ninthDigit && (
+			       ($length == 9 && (!in_array($number[1], $this->cellphoneBegin) || $number[0] != 9)) ||
+		           ($length == 8 && (!in_array($number[0], $dontHaveNinthDigit)))) ||
+			   (!$ninthDigit && (
+				   ($area && !in_array($number[0], $this->cellphoneBegin)) ||
+				   (!$area && (
+					   ($length == 9 && (!in_array($number[1], $this->cellphoneNinthDigitBegin) || $number[0] != 9)) ||
+					   ($length == 8 && (!in_array($number[0], $this->cellphoneBegin))))
+				   )
+			   ))
+		) {
+			$this->errors[] = $this->message;
+			return false;
+		}
+
+		return true;
+	}
+
+	protected function validateBoth($area, $number) {
+		if ($this->validateLandline($area, $number)) {
+			return true;
+		}
+		else{
+			$landline_errors = $this->errors;
+			$this->errors = array();
+			$valid_cellphone = $this->validateCellphone($area, $number);
+			if (!$valid_cellphone) {
+				foreach($landline_errors as $error) {
+					if (!in_array($error, $this->errors))
+						$this->errors[] = $error;
+				}
+			}
+		}
+	}
+}
---
+++ tests/PhoneValidatorTest.php
@@ -0,0 +1,78 @@
+<?php
+require_once 'TestCase.php';
+
+class PhoneValidatorTest extends TestCase {
+
+	public function testEmpty() {
+		$this->guy->cellphone = '';
+		$this->assertValid();
+	}
+
+	public function testLength() {
+		$this->guy->cellphone = '984-8888';
+		$this->assertInvalid();
+		$this->assertModelErrorIs('cellphone', 'Cellphone de tamanho inválido.');
+		$this->guy->cellphone = '9848888';
+		$this->assertInvalid();
+		$this->guy->cellphone = '';
+
+		$this->assertModelErrorIs('cellphone', 'Cellphone de tamanho inválido.');
+		$this->guy->landline = '281-7456';
+		$this->assertInvalid();
+		$this->assertModelErrorIs('landline', 'Landline de tamanho inválido.');
+		$this->guy->landline = '2817456';
+		$this->assertInvalid();
+		$this->assertModelErrorIs('landline', 'Landline de tamanho inválido.');
+		$this->guy->landline = '22281-7456';
+		$this->assertInvalid();
+		$this->assertEquals(array('landline' => array('Landline is invalid.','Landline de tamanho inválido.')), $this->guy->errors);
+	}
+
+	public function testNinthDigit() {
+		BrazilianPerson::$customRules['cellphone'] = array_merge(BrazilianPerson::$defaultRules['cellphone'], array('areaCode' => true));
+		$this->guy->cellphone = '(21) 99999-9999';
+		$this->assertValid();
+
+		BrazilianPerson::$customRules['cellphone'] = array_merge(BrazilianPerson::$defaultRules['cellphone'], array('areaCode' => true));
+		$this->guy->cellphone = '(11) 7999-9999';
+		$this->assertValid();
+
+		BrazilianPerson::$customRules['cellphone'] = array_merge(BrazilianPerson::$defaultRules['cellphone'], array('areaCode' => true));
+		$this->guy->cellphone = '(11) 9999-9999';
+		$this->assertInvalid();
+		$this->assertEquals(array('cellphone' => array('Falta o nono dígito no Cellphone.', 'Cellphone is invalid.')), $this->guy->errors);
+	}
+
+	public function testInvalid() {
+		BrazilianPerson::$customRules['landline'] = array_merge(BrazilianPerson::$defaultRules['landline'], array('type' => PhoneValidator::TYPE_LANDLINE));
+		$this->guy->landline = '6789-9999';
+		$this->assertInvalid();
+		$this->assertModelErrorIs('landline', 'Landline is invalid.');
+		$this->guy->landline = '';
+
+		BrazilianPerson::$customRules['cellphone'] = array_merge(BrazilianPerson::$defaultRules['cellphone'], array('type' => PhoneValidator::TYPE_CELLPHONE));
+		$this->guy->cellphone = '6789-9999';
+		$this->assertInvalid();
+		$this->assertEquals(array('cellphone' => array('Cellphone de tamanho inválido.')), $this->guy->errors);
+
+		//here it should warn for any of the two validators
+		$this->guy->cellphone = '6789-9999';
+		$this->assertInvalid();
+		$this->assertEquals(array('cellphone' => array('Cellphone de tamanho inválido.')), $this->guy->errors);
+
+		$this->guy->cellphone = '97789-9999';
+		$this->assertInvalid();
+		$this->assertEquals(array('cellphone' => array('Cellphone de tamanho inválido.')), $this->guy->errors);
+	}
+
+	public function testValid() {
+		$this->guy->landline = '2281-2222';
+		$this->assertValid();
+		$this->guy->cellphone = '9999-9999';
+		$this->assertValid();
+		$this->guy->cellphone = '7999-9999';
+		$this->assertValid();
+		$this->guy->cellphone = '99999-9999';
+		$this->assertValid();
+	}
+}
